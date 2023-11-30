73ff97b7d52ca4f8bb31fd8320e68ae08af4b427
Eddie <callenberg99@gmail.com>
2023-11-30 14:18:04 +0100

    Add project files.

---
+++ InterfacesPractice/Interfaces/IBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesPractice.Interfaces
+{
+    internal interface IBook
+    {
+        string Title { get; set; }
+        string Author { get; set; }
+        void PrintContent();
+    }
+}
